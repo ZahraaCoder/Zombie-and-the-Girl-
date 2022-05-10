3bb6329713233de3185564b87cd52a7dcd7c09c2
ZahraaCoder <96212566+ZahraaCoder@users.noreply.github.com>
2022-05-10 19:55:36 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,11 @@ var zombie,zombierunning,zombieattack;
 
 function preload(){
 bgImg=loadImage("BG.jpg");
-girlrunning=loadAnimation("assets\Run (1).png","assets\Run (2).png","assets\Run (3).png","assets\Run (4).png","assets\Run (5).png","assets\Run (6).png","assets\Run (7).png","assets\Run (8).png","assets\Run (9).png","assets\Run (10).png","assets\Run (11).png","assets\Run (12).png","assets\Run (13).png","assets\Run (14).png","assets\Run (15).png","assets\Run (16).png","assets\Run (17).png","assets\Run (18).png","assets\Run (19).png","assets\Run (20).png");
+girlrunning=loadAnimation("assets/Run (1).png","assets/Run (2).png","assets/Run (3).png","assets/Run (4).png","assets/Run (5).png","assets/Run (6).png","assets/Run (7).png","assets/Run (8).png","assets/Run (9).png","assets/Run (10).png","assets/Run (11).png");
+zombierunning=loadAnimation("assets/Walk (1).png","assets/Walk (2).png","assets/Walk (3).png","assets/Walk (4).png","assets/Walk (5).png","assets/Walk (6).png","assets/Walk (7).png","assets/Walk (8).png","assets/Walk (9).png","assets/Walk (10).png");
+ 
+
+
 
 }
 
@@ -14,6 +18,14 @@ bg=createSprite(750,360);
 bg.addImage(bgImg);
 bg.scale=1.6;
 bg.velocityX=-2;
+
+girl=createSprite(900,600);
+girl.addAnimation("running",girlrunning);
+girl.scale=0.4;
+
+zombie=createSprite(230,550);
+zombie.addAnimation("running",zombierunning);
+zombie.scale=0.5;
 
 }
 
